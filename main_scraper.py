26a99161538445a1cc5d8ed10db783cd17fa3c42
regutierrez <11924623+regutierrez@users.noreply.github.com>
2024-03-18 18:15:36 -0400

    comment out user agent as it's not needed

--- main_scraper.py
+++ main_scraper.py
@@ -2,18 +2,22 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-def spotontrack_login():
+def spotontrack_login(credentials: dict[str, str]) -> requests.Response:
     login_url: str = "https://www.spotontrack.com/login"
     username: str = "gutierrez.rafael23e@gmail.com"
     password: str = "decode-armoire-recopy9-donut-unmanaged"
+
+    # might need to add this as arg for session.get in the future
+    """
     user_agent: str = (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.142.86 Safari/537.36"
     )
 
     headers: dict[str, str] = {"User-Agent": user_agent}
+    """
 
     with requests.Session() as session:
-        response_content: str = session.get(login_url, headers=headers)._content
+        response_content: str = session.get(login_url)._content
         html: bs = bs(response_content, "html.parser")
         auth_token: str = html.find("input", attrs={"name": "_token"}).attrs["value"]
 
